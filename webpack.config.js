363e2420c8c44bcd5ffdd2005daf27c7b5dc0894
uuyeong <proyeongs@gmail.com>
2024-01-01 21:50:30 +0900

    웹팩 플러그인 실습

--- webpack.config.js
+++ webpack.config.js
@@ -1,30 +1,56 @@
 const path = require("path");
-
+const webpack = require("webpack");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 module.exports = {
   mode: "development",
   entry: {
-    main: "./src/app.js"
+    main: "./src/app.js",
   },
   output: {
     filename: "[name].js",
-    path: path.resolve("./dist")
+    path: path.resolve("./dist"),
   },
   module: {
     rules: [
       {
         test: /\.css$/,
-        use: ["style-loader", "css-loader"]
+        use: [MiniCssExtractPlugin.loader, "css-loader"],
       },
       {
         test: /\.(png|jpg|svg|gif)$/,
         loader: "url-loader",
         options: {
           name: "[name].[ext]?[hash]",
-          limit: 10000 // 10Kb
-        }
-      }
-    ]
-  }
+          limit: 10000, // 10Kb
+        },
+      },
+    ],
+  },
+  plugins: [
+    new webpack.BannerPlugin({
+      banner: (bundlers) => {
+        const date = new Date().toLocaleString();
+        console.log(process.env);
+        return `Build Time : ${date}`;
+      },
+    }),
+    new HtmlWebpackPlugin({
+      template: "./src/index.html",
+      templateParameters: {
+        env: process.env.NODE_ENV === "development" ? "(개발용)" : "프로덕션",
+      },
+      minify: {
+        removeComments: true, // 주석 제거
+      },
+    }),
+    new CleanWebpackPlugin(),
+    new MiniCssExtractPlugin({ filename: `[name].css` }),
+    new webpack.DefinePlugin({
+      NODE_ENV: "development",
+    }),
+  ],
   /**
    * TODO: 아래 플러그인을 추가해서 번들 결과를 만들어 보세요.
    * 1. BannerPlugin: 결과물에 빌드 시간을 출력하세요.
